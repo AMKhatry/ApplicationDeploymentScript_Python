5a12979ce6637f9075f457d86b3274799a265cef
AdityaK <amkhatry@gmail.com>
2024-02-13 12:22:50 +1100

    Implemented script to create a backup of a specified folder while excluding the nested 'Backup' directory during the copying process.

--- DeploymentScript.py
+++ DeploymentScript.py
@@ -2,10 +2,26 @@ import os
 import shutil
 import datetime
 
-# C:\Users\User\Aditya\Personal\New folder\Equipcom
-dateTimeString = datetime.datetime.now().strftime("%d%m%Y_%H%M")
-deployedAppLocation = input("Enter the deployed application path: ")
-backupDestination = os.path.join(deployedAppLocation, f"Backup\\Equipcom_{dateTimeString}")
 
-# shutil.copy2(deployedAppLocation, backupDestination)
+def backup_folder(deployed_app_location, backup_destination):
+    try:
+        # Function to determine which files to ignore during copy
+        def ignore_backup_folder(directory, contents):
+            return ['Backup'] if 'Backup' in contents else []
+        
+        shutil.copytree(deployed_app_location, backup_destination, ignore=ignore_backup_folder)
+        print("Backup successful!")
+    except Exception as e:
+        print(f"Backup failed: {e}")
 
+
+def main():
+    deployed_app_location = input("Enter the deployed application path: ")
+    
+    date_time_string = datetime.datetime.now().strftime("%d%m%Y_%H%M")
+    
+    backup_destination = os.path.join(deployed_app_location, f"Backup\\Equipcom_{date_time_string}")
+
+    backup_folder (deployed_app_location, backup_destination)
+
+main()
